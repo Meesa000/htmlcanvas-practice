5697946333c7de7bf1f9d64f074eb9828ac2d324
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-02-20 15:11:37 +0000

    initial

---
+++ main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('mainCanvas');
+const ctx = canvas.getContext('2d');
+let width = canvas.width = 1024;
+let height = canvas.height = 576;
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10,10,width,height);
+
