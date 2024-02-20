c08648bce789dadb0c511283ff4ddbf1dab54001
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-02-20 16:30:47 +0000

    add player class and add update and draw functions to clean up code

--- main.js
+++ main.js
@@ -3,22 +3,51 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-let x = 100;
-let y = 100;
+class Player {
+    constructor(){
+        this.position = {
+            x: 100,
+            y: 100
+        }
+
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom:this.position.y + this.height
+
+
+          }
+        }
+
+    draw(){
+        ctx.fillStyle = 'green ';
+        ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
+
+    }
+
+    update(){
+        if (this.sides.bottom < canvas.height) {
+            this.position.y++;
+            this.sides.bottom = this.position.y + this.height;
+
+        }
+
+    }
+}   
+
+const player = new Player();
+
+// let bottom = y + playerHeight;
 
 function animate() {
     window.requestAnimationFrame(animate);
     ctx.fillStyle = 'white';
     ctx.fillRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle = 'green ';
-    ctx.fillRect(x,y,100,100);
-    if (y == canvas.height - 100 ){
-        y = canvas.height - 100;
-    }
-    else{
-        y++;
-    }
-    
+   
+    player.draw();
+    player.update();
+
+   
 
 }
 
